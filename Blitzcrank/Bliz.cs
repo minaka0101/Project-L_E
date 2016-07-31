748aac47b0688be620f4f764a95ab85b0643f317
minaka0101 <phamquan@hotmail.com>
2016-07-31 09:36:53 +0700

    Initial commit

---
+++ Blitzcrank/Bliz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+using Utility = LeagueSharp.Common.Utility;
+using Spell = LeagueSharp.Common.Spell;
+using TargetSelector = LeagueSharp.Common.TargetSelector;
+using SharpDX;
+
+namespace Blitzcrank
+{
+    internal class Bliz
+    {
+        internal static Menu Root;
+        internal static Random Rand;
+        internal static Spell Q, W, E, R;
+        internal static Orbwalking.Orbwalker Orbwalker;
+        internal static AIHeroClient Player = ObjectManager.Player;
+        public Bliz()
+        {
+            
+            Setmenu();
+        }
+
+        private static void Setmenu()
+        {
+            
+        }
+    }
+}
